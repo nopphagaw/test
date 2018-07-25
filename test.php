42b79219a0118c2cb499ba169ada104aa640c6ac
nopphagaw <40820661+nopphagaw@users.noreply.github.com>
2018-07-25 20:55:19 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,3 +1,18 @@
 <?php
 echo "test";
 ?>
+
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
