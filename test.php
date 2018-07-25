0dca0a652c41af937cbbf5c7e8eadb2cf3efec88
nopphagaw <40820661+nopphagaw@users.noreply.github.com>
2018-07-26 04:45:34 +0700

    Update test.php

--- test.php
+++ test.php
@@ -5,9 +5,9 @@ echo "test 1";
 <?php
 $dbhost = getenv("MYSQL_SERVICE_HOST");
 $dbport = getenv("MYSQL_SERVICE_PORT");
-$dbuser = getenv("databaseuser");
-$dbpwd = getenv("databasepassword");
-$dbname = getenv("databasename");
+$dbuser = getenv("MYSQL_USER");
+$dbpwd = getenv("MYSQL_PASSWORD");
+$dbname = getenv("sampledb");
 
 $connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
 if ($connection->connect_errno) {
