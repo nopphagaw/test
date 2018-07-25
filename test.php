93c321e72d90f5862701aff9bc9144b7b1d33718
nopphagaw <40820661+nopphagaw@users.noreply.github.com>
2018-07-26 06:23:50 +0700

    Update test.php

--- test.php
+++ test.php
@@ -4,11 +4,11 @@ echo "test 1";
 
 <?php
 $dbhost = "mysql";
-$dbport = "3306";
+$dbport = 3306;
 $dbuser = "user5YW";
 $dbpwd = "k45WnAQoojrkj01Y";
 $dbname = "sampledb";
-
+echo $dbhost.":".$dbport.":".$dbuser.":".$dbpwd;
 $connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
 if ($connection->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
